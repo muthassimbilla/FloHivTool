234422e919c16282684da84e2066e4a24605aa9e
muthassimbilla <122305833+muthassimbilla@users.noreply.github.com>
2025-09-20 18:50:36 +0600

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,22 +3,23 @@ import { getAuth } from "firebase/auth"
 import { env } from "./env"
 
 const isFirebaseConfigured = () => {
-  console.log("[v0] Firebase environment variables check:")
-  console.log("[v0] API_KEY:", !!env.NEXT_PUBLIC_FIREBASE_API_KEY)
-  console.log("[v0] AUTH_DOMAIN:", !!env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN)
-  console.log("[v0] PROJECT_ID:", !!env.NEXT_PUBLIC_FIREBASE_PROJECT_ID)
-  console.log("[v0] STORAGE_BUCKET:", !!env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET)
-  console.log("[v0] MESSAGING_SENDER_ID:", !!env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID)
-  console.log("[v0] APP_ID:", !!env.NEXT_PUBLIC_FIREBASE_APP_ID)
+  const apiKey = env.NEXT_PUBLIC_FIREBASE_API_KEY || process.env.NEXT_PUBLIC_FIREBASE_API_KEY
+  const authDomain = env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN
+  const projectId = env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID
+  const storageBucket = env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET
+  const messagingSenderId =
+    env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID
+  const appId = env.NEXT_PUBLIC_FIREBASE_APP_ID || process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 
-  const configured = !!(
-    env.NEXT_PUBLIC_FIREBASE_API_KEY &&
-    env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN &&
-    env.NEXT_PUBLIC_FIREBASE_PROJECT_ID &&
-    env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET &&
-    env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID &&
-    env.NEXT_PUBLIC_FIREBASE_APP_ID
-  )
+  console.log("[v0] Firebase environment variables check:")
+  console.log("[v0] API_KEY:", !!apiKey)
+  console.log("[v0] AUTH_DOMAIN:", !!authDomain)
+  console.log("[v0] PROJECT_ID:", !!projectId)
+  console.log("[v0] STORAGE_BUCKET:", !!storageBucket)
+  console.log("[v0] MESSAGING_SENDER_ID:", !!messagingSenderId)
+  console.log("[v0] APP_ID:", !!appId)
+
+  const configured = !!(apiKey && authDomain && projectId && storageBucket && messagingSenderId && appId)
 
   console.log("[v0] Firebase configured:", configured)
   return configured
@@ -30,12 +31,13 @@ let auth: any = null
 if (isFirebaseConfigured()) {
   try {
     const firebaseConfig = {
-      apiKey: env.NEXT_PUBLIC_FIREBASE_API_KEY!,
-      authDomain: env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
-      projectId: env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
-      storageBucket: env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
-      messagingSenderId: env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
-      appId: env.NEXT_PUBLIC_FIREBASE_APP_ID!,
+      apiKey: env.NEXT_PUBLIC_FIREBASE_API_KEY || process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+      authDomain: env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+      projectId: env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+      storageBucket: env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+      messagingSenderId:
+        env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+      appId: env.NEXT_PUBLIC_FIREBASE_APP_ID || process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
     }
 
     // Initialize Firebase
