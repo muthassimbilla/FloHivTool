ca7062ed75e5add290750447636bb4242593f505
muthassimbilla <122305833+muthassimbilla@users.noreply.github.com>
2025-09-22 15:56:11 +0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -50,7 +50,7 @@ const nextConfig = {
           },
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline' fonts.googleapis.com; font-src 'self' fonts.gstatic.com; img-src 'self' data: blob: https:; connect-src 'self' https:; frame-ancestors 'none'; worker-src 'self';"
+            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' embed.tawk.to; style-src 'self' 'unsafe-inline' fonts.googleapis.com; font-src 'self' fonts.gstatic.com; img-src 'self' data: blob: https:; connect-src 'self' https: wss: embed.tawk.to; frame-ancestors 'none'; worker-src 'self';"
           },
           {
             key: 'Strict-Transport-Security',
